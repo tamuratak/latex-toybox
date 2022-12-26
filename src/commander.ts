5cbc1208ce5b34072829c7b7dd76641805b4fc31
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-12-27 08:25:28 +0900

    Remove src/components/counter.ts

--- src/commander.ts
+++ src/commander.ts
@@ -236,20 +236,6 @@ export class Commander {
         return this.extension.completer.citation.browser()
     }
 
-    wordcount() {
-        this.extension.logger.addLogMessage('WORDCOUNT command invoked.')
-        if (!vscode.window.activeTextEditor || !this.extension.manager.hasTexId(vscode.window.activeTextEditor.document.languageId) ||
-            this.extension.manager.rootFile === vscode.window.activeTextEditor.document.fileName) {
-            if (this.extension.manager.rootFile) {
-                this.extension.counter.count(this.extension.manager.rootFile)
-            } else {
-                this.extension.logger.addLogMessage('WORDCOUNT: No rootFile defined.')
-            }
-        } else {
-            this.extension.counter.count(vscode.window.activeTextEditor.document.fileName, false)
-        }
-    }
-
     log(compiler?: string) {
         this.extension.logger.addLogMessage(`LOG command invoked: ${compiler || 'default'}`)
         if (compiler) {
