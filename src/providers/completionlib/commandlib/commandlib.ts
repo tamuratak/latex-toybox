3d64e0ac299368c791614de4b6687d50eeb74f8a
Takashi Tamura <tamuratak@users.noreply.github.com>
2023-09-11 07:41:40 +0900

    edit doc [skip ci]

--- src/providers/completionlib/commandlib/commandlib.ts
+++ src/providers/completionlib/commandlib/commandlib.ts
@@ -3,6 +3,12 @@ import { existsPath } from '../../../lib/lwfs/lwfs'
 import {CmdEnvSuggestion} from '../command'
 
 
+/**
+ * Dertermine if intellisense should be triggered after the given command is inserted,
+ * e.g. \input{}, \cite{}, \ref{}, etc.
+ * @param name command name
+ * @returns true if intellisense should be triggered, false otherwise.
+ */
 export function isTriggerSuggestNeeded(name: string): boolean {
     const reg = /^(?:[a-z]*(cite|ref|input)[a-z]*|begin|bibitem|(sub)?(import|includefrom|inputfrom)|gls(?:pl|text|first|plural|firstplural|name|symbol|desc|user(?:i|ii|iii|iv|v|vi))?|Acr(?:long|full|short)?(?:pl)?|ac[slf]?p?)/i
     return reg.test(name)
