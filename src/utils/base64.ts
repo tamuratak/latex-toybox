b75391a398fd10f7e1006915413e33a19e6378cc
Takashi Tamura <tamuratak@users.noreply.github.com>
2023-10-31 07:40:13 +0900

    Add base64.ts.

---
+++ src/utils/base64.ts
@@ -0,0 +1,35 @@
+const encoder = new TextEncoder()
+/**
+ * Copy from https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem
+ */
+export function encodeToBase64(str: string) {
+    const bytes = encoder.encode(str)
+    const binString = String.fromCodePoint(...bytes)
+    return globalThis.btoa(binString)
+}
+
+const decoder = new TextDecoder()
+/**
+ * Copy from https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem
+ */
+export function decodeFromBase64(base64: string) {
+    const binString = globalThis.atob(base64)
+    const bytes = Uint8Array.from(binString, (m) => {
+        const result = m.codePointAt(0)
+        if (result === undefined) {
+            throw new Error('Invalid base64 string')
+        }
+        return result
+    })
+    return decoder.decode(bytes)
+}
+
+export function encodeToBase64Url(str: string) {
+    return encodeToBase64(str).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
+}
+
+export function decodeFromBase64Url(b64url: string) {
+    const tmp = b64url + '='.repeat(4 - b64url.length % 4)
+    const b64 = tmp.replace(/-/g, '+').replace(/_/g, '/')
+    return decodeFromBase64(b64)
+}
--- src/utils/svg.ts
+++ src/utils/svg.ts
@@ -1,5 +1,7 @@
+import { encodeToBase64 } from './base64'
+
 export function svgToDataUrl(xml: string): string {
-    const svg64 = Buffer.from(xml).toString('base64')
+    const svg64 = encodeToBase64(xml)
     const b64Start = 'data:image/svg+xml;base64,'
     return b64Start + svg64
 }
