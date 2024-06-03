08e74bd625de77a8fb0b0d00a5cbae20ec5a8e9e
Takashi Tamura <tamuratak@users.noreply.github.com>
2024-06-03 16:18:59 +0900

    Enable features on Copilot Chat Panel.

--- src/utils/selectdocument.ts
+++ src/utils/selectdocument.ts
@@ -2,7 +2,7 @@ import type * as vscode from 'vscode'
 
 export function selectDocumentsWithId(ids: string[]): vscode.DocumentSelector {
     const selector = ids.map( (id) => {
-        return { scheme: 'file', language: id }
+        return { language: id }
     })
     return selector
 }
