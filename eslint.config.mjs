07ea9123bf2c46fdf970a5c749df59455b90511b
Takashi Tamura <tamuratak@users.noreply.github.com>
2024-08-11 13:47:17 +0900

    Add common ESLint rules for Node.js and browser environments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,88 @@
 import eslint from "@eslint/js";
 import tseslint from 'typescript-eslint';
 
+const commonRules = {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/consistent-type-assertions": ["error", {
+        assertionStyle: "as",
+        objectLiteralTypeAssertions: "never",
+    }],
+    "@typescript-eslint/no-empty-interface": ["error", {
+        allowSingleExtends: true,
+    }],
+    "@typescript-eslint/no-floating-promises": ["error", {
+        checkThenables: true,
+    }],
+    "@typescript-eslint/no-invalid-void-type": "error",
+    "@typescript-eslint/no-misused-promises": ["error", {
+        checksVoidReturn: {
+            arguments: false,
+        },
+    }],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/no-unsafe-argument": "error",
+    "@typescript-eslint/no-unsafe-assignment": "error",
+    "@typescript-eslint/no-unsafe-call": "error",
+    "@typescript-eslint/no-unsafe-return": "error",
+    "no-unused-expressions": "off",
+    "@typescript-eslint/no-unused-expressions": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+            "args": "all",
+            "argsIgnorePattern": "^_",
+            "caughtErrors": "all",
+            "caughtErrorsIgnorePattern": "^_",
+            "destructuredArrayIgnorePattern": "^_",
+            "varsIgnorePattern": "^_",
+            "ignoreRestSiblings": true
+        }
+    ],
+    "@typescript-eslint/no-require-imports": "error",
+    "@typescript-eslint/prefer-includes": "error",
+    "@typescript-eslint/prefer-readonly": "error",
+    "no-return-await": "off",
+    "@typescript-eslint/return-await": "error",
+    "require-await": "off",
+    "@typescript-eslint/require-await": "error",
+    "@typescript-eslint/unbound-method": "error",
+    "curly": "error",
+    "default-case": "error",
+    "eol-last": "error",
+    "eqeqeq": ["error", "always"],
+    "func-call-spacing": ["error", "never"],
+    "no-caller": "error",
+    "no-constant-condition": "error",
+    "no-eval": "error",
+    "no-invalid-this": "error",
+    "no-multiple-empty-lines": "error",
+    "no-multi-spaces": "error",
+    "no-new-wrappers": "error",
+    "no-trailing-spaces": "error",
+    "no-empty": ["error", {
+        allowEmptyCatch: true,
+    }],
+    "object-shorthand": "error",
+    "one-var": ["error", {
+        initialized: "never",
+        uninitialized: "never",
+    }],
+    "prefer-arrow-callback": ["error", {
+        allowUnboundThis: false,
+    }],
+    "quotes": ["error", "single", {
+        avoidEscape: true,
+    }],
+    "space-before-function-paren": ["error", {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+    }],
+};
+
 export default tseslint.config(
     {
         ignores: [
@@ -40,13 +122,12 @@ export default tseslint.config(
             },
         },
         rules: {
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-empty-function": "off",
-            "@typescript-eslint/naming-convention": ["error", {
-                selector: "default",
-                format: ["camelCase", "PascalCase", "UPPER_CASE"],
-                leadingUnderscore: "allow",
-            }, {
+            "@typescript-eslint/naming-convention": ["error",
+                {
+                    selector: "default",
+                    format: ["camelCase", "PascalCase", "UPPER_CASE"],
+                    leadingUnderscore: "allow",
+                }, {
                     selector: "method",
                     format: ["camelCase"],
                 }, {
@@ -59,83 +140,7 @@ export default tseslint.config(
                     selector: "objectLiteralProperty",
                     format: null,
                 }],
-            "@typescript-eslint/consistent-type-assertions": ["error", {
-                assertionStyle: "as",
-                objectLiteralTypeAssertions: "never",
-            }],
-            "@typescript-eslint/no-empty-interface": ["error", {
-                allowSingleExtends: true,
-            }],
-            "@typescript-eslint/no-floating-promises": ["error", {
-                checkThenables: true,
-            }],
-            "@typescript-eslint/no-invalid-void-type": "error",
-            "@typescript-eslint/no-misused-promises": ["error", {
-                checksVoidReturn: {
-                    arguments: false,
-                },
-            }],
-            "no-shadow": "off",
-            "@typescript-eslint/no-shadow": "error",
-            "@typescript-eslint/no-unsafe-argument": "error",
-            "@typescript-eslint/no-unsafe-assignment": "error",
-            "@typescript-eslint/no-unsafe-call": "error",
-            "@typescript-eslint/no-unsafe-return": "error",
-            "no-unused-expressions": "off",
-            "@typescript-eslint/no-unused-expressions": "error",
-            "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": [
-                "error",
-                {
-                    "args": "all",
-                    "argsIgnorePattern": "^_",
-                    "caughtErrors": "all",
-                    "caughtErrorsIgnorePattern": "^_",
-                    "destructuredArrayIgnorePattern": "^_",
-                    "varsIgnorePattern": "^_",
-                    "ignoreRestSiblings": true
-                }
-            ],
-            "@typescript-eslint/no-require-imports": "error",
-            "@typescript-eslint/prefer-includes": "error",
-            "@typescript-eslint/prefer-readonly": "error",
-            "no-return-await": "off",
-            "@typescript-eslint/return-await": "error",
-            "require-await": "off",
-            "@typescript-eslint/require-await": "error",
-            "@typescript-eslint/unbound-method": "error",
-            "curly": "error",
-            "default-case": "error",
-            "eol-last": "error",
-            "eqeqeq": ["error", "always"],
-            "func-call-spacing": ["error", "never"],
-            "no-caller": "error",
-            "no-constant-condition": "error",
-            "no-eval": "error",
-            "no-invalid-this": "error",
-            "no-multiple-empty-lines": "error",
-            "no-multi-spaces": "error",
-            "no-new-wrappers": "error",
-            "no-trailing-spaces": "error",
-            "no-empty": ["error", {
-                allowEmptyCatch: true,
-            }],
-            "object-shorthand": "error",
-            "one-var": ["error", {
-                initialized: "never",
-                uninitialized: "never",
-            }],
-            "prefer-arrow-callback": ["error", {
-                allowUnboundThis: false,
-            }],
-            "quotes": ["error", "single", {
-                avoidEscape: true,
-            }],
-            "space-before-function-paren": ["error", {
-                anonymous: "always",
-                named: "never",
-                asyncArrow: "always",
-            }],
+            ...commonRules,
         },
     },
     {
@@ -154,6 +159,7 @@ export default tseslint.config(
                 format: ["PascalCase"],
             }],
             "@typescript-eslint/ban-ts-comment": "off",
+            ...commonRules
         },
     }
 )
